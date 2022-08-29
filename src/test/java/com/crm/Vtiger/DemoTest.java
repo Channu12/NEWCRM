e2c8657800f528decee274b1ecbf1702bf6d2bb7
User <User@M-SHARATH-KUMAR>
2022-08-29 12:31:11 +0530

    THIS IS NEW CRM S/W

---
+++ src/test/java/com/crm/Vtiger/DemoTest.java
@@ -0,0 +1,12 @@
+package com.crm.Vtiger;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void display()
+	{
+		System.out.println("====Hi...====");
+	}
+}
---
+++ src/test/java/com/crm/Vtiger/SampleTest.java
@@ -0,0 +1,8 @@
+package com.crm.Vtiger;
+
+public class SampleTest {
+public static void main(String[] args) {
+	
+	System.out.println("=====Done=====");
+}
+}
